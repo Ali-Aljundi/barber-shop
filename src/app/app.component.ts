59f2cb3bde44e9703eb11dbad36b28636d0949ac
Marah Daher <marah.daher193@outlook.com>
2022-03-12 21:31:09 +0200

    new add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    AOS.refresh()
+  }
   title = 'barber-shop';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+
 }
+ 
