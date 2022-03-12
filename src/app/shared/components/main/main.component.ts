842faf02dcaba782bd20180516dc593d9f523bf4
ali_aljonde <alialjonde6@gmail.com>
2022-03-12 02:31:11 +0200

    init project

--- src/app/shared/components/main/main.component.ts
+++ src/app/shared/components/main/main.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from '../base.component';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(injector: Injector,
+  ) {
+    super(injector);
+
+  }
 
   ngOnInit(): void {
   }
